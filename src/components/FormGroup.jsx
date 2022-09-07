d5c04033f705551fb9129466f242f0111098f524
Sruthi Suresh <sruthisuresh321@gmail.com>
2022-09-07 17:13:43 +0530

    Add form group component and styles

---
+++ src/components/FormGroup.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const FormGroup = ({
+  error,
+  fixedInput,
+  formLabel,
+  inputPlaceholder,
+  optional = false,
+}) => {
+  return (
+    <div className="form-group">
+      <label className="form-group__label" htmlFor={formLabel}>
+        {formLabel}
+        {optional && <span className="form-group__optional">(optional)</span>}
+      </label>
+      <div className="form-group__input-container">
+        {fixedInput && <p className="form-group__fixed-input">{fixedInput}</p>}
+        <input
+          className="form-group__input"
+          data-error={error ? true : false}
+          id={formLabel}
+          placeholder={inputPlaceholder}
+          type="text"
+        />
+      </div>
+      {error && <p className="form-group__error-message">Please enter this field</p>}
+    </div>
+  );
+};
+
+export default FormGroup;
